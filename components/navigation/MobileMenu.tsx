37fbc60fbc83d3dcb0c31bf403f602860778e647
delude88 <tobias.hegemann@googlemail.com>
2020-10-22 15:29:19 +0200

    Added mobile nav menu

--- components/navigation/MobileMenu.tsx
+++ components/navigation/MobileMenu.tsx
@@ -16,6 +16,8 @@ import MenuIcon from '@material-ui/icons/Menu';
 import {useAuth} from "../../lib/digitalstage/useAuth";
 import {AccountCircle} from "@material-ui/icons";
 import {useRouter} from "next/router";
+import useStageSelector from "../../lib/digitalstage/useStageSelector";
+import {Stage} from "../../lib/digitalstage/common/model.server";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -34,12 +36,15 @@ const useStyles = makeStyles((theme) => ({
 const MobileMenu = (props: {
     className: string
 }) => {
-    const {user} = useAuth();
     const classes = useStyles();
     const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
     const [menuOpen, setMenuOpen] = useState<boolean>(false);
     const router = useRouter();
+
+    const {user} = useAuth();
+
+    const stage = useStageSelector<Stage>(state => state.stageId ? state.stages.byId[state.stageId] : undefined);
 
     const handleMenu = (event) => {
         setAnchorEl(event.currentTarget);
@@ -102,34 +107,46 @@ const MobileMenu = (props: {
                     )}
                 </Toolbar>
             </AppBar>
-            <Drawer className={props.className} anchor="left" open={menuOpen} onClose={() => setMenuOpen(false)}>
-                <List className={classes.list}>
-                    <ListItem button onClick={() => {
-                        router.push("/")
-                            .then(() => setMenuOpen(false))
-                    }}>
-                        <ListItemText>My stage</ListItemText>
-                    </ListItem>
-                    <ListItem button onClick={() => {
-                        router.push("/mixer")
-                            .then(() => setMenuOpen(false))
-                    }}>
-                        <ListItemText>MIXER</ListItemText>
-                    </ListItem>
-                    <ListItem button onClick={() => {
-                        router.push("/stages")
-                            .then(() => setMenuOpen(false))
-                    }}>
-                        <ListItemText>STAGES</ListItemText>
-                    </ListItem>
-                    <ListItem button onClick={() => {
-                        router.push("/devices")
-                            .then(() => setMenuOpen(false))
-                    }}>
-                        <ListItemText>DEVICES</ListItemText>
-                    </ListItem>
-                </List>
-            </Drawer>
+            {user && (
+                <Drawer className={props.className} anchor="left" open={menuOpen} onClose={() => setMenuOpen(false)}>
+                    <List className={classes.list}>
+                        {stage && (
+                            <>
+                                <ListItem button onClick={() => {
+                                    router.push("/")
+                                        .then(() => setMenuOpen(false))
+                                }}>
+                                    <ListItemText>{stage.name}</ListItemText>
+                                </ListItem>
+                                <ListItem button onClick={() => {
+                                    router.push("/mixer")
+                                        .then(() => setMenuOpen(false))
+                                }}>
+                                    <ListItemText>Mischpult</ListItemText>
+                                </ListItem>
+                                <ListItem button onClick={() => {
+                                    router.push("/leave")
+                                        .then(() => setMenuOpen(false))
+                                }}>
+                                    <ListItemText>Bühne verlassen</ListItemText>
+                                </ListItem>
+                            </>
+                        )}
+                        <ListItem button onClick={() => {
+                            router.push("/stages")
+                                .then(() => setMenuOpen(false))
+                        }}>
+                            <ListItemText>Meine Bühnen</ListItemText>
+                        </ListItem>
+                        <ListItem button onClick={() => {
+                            router.push("/devices")
+                                .then(() => setMenuOpen(false))
+                        }}>
+                            <ListItemText>Meine Geräte</ListItemText>
+                        </ListItem>
+                    </List>
+                </Drawer>
+            )}
         </>
     )
 }
