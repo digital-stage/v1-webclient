19dc31444725fbdf65c26e8a9489fae21bca0cbb
delude88 <tobias.hegemann@googlemail.com>
2020-11-04 18:31:51 +0100

    Updated sidebar

---
+++ components/new/Container/index.tsx
@@ -0,0 +1,13 @@
+import { styled } from 'baseui';
+
+const Container = styled('div', {
+  display: 'flex',
+  width: '100%',
+  maxWidth: '800px',
+  paddingLeft: '15px',
+  paddingRight: '15px',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  flexDirection: 'column',
+});
+export default Container;
