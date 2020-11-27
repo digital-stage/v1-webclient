408222eacdc8d9db96d55fb72e630125d28245a3
Harald T. Wirths <harald.wirths@4fb.de>
2020-11-27 10:16:21 +0100

    fix: enlarged width to show all buttons merged components

--- components/StageDeviceController.tsx
+++ components/StageDeviceController.tsx
@@ -1,18 +1,42 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import Link from 'next/link';
-import { FaMicrophone, FaMicrophoneSlash, FaVideo, FaVideoSlash } from 'react-icons/fa';
-import { ImPhoneHangUp } from 'react-icons/im';
+import { useCallback, useEffect, useState } from 'react';
 import { BsThreeDotsVertical } from 'react-icons/bs';
-import { Button, Flex, jsx, Box } from 'theme-ui';
-import FixedAudioPlaybackStarterButton from './new/elements/Menu/FixedAudioPlaybackStarterButton';
+import { FaMicrophone, FaMicrophoneSlash, FaVideo, FaVideoSlash } from 'react-icons/fa';
+import { GiSpeaker, GiSpeakerOff } from 'react-icons/gi';
+import { ImPhoneHangUp } from 'react-icons/im';
+import { Box, Button, Flex, jsx } from 'theme-ui';
 import { useLocalDevice } from '../lib/use-digital-stage/hooks';
 import useStageActions from '../lib/use-digital-stage/useStageActions';
+import { useAudioContext } from '../lib/useAudioContext';
 
 const StageDeviceController = (): JSX.Element => {
-  // TODO: @delude88 - please check
   const localDevice = useLocalDevice();
   const { updateDevice } = useStageActions();
+  const { audioContext, createAudioContext } = useAudioContext();
+  const [valid, setValid] = useState<boolean>(audioContext && audioContext.state === 'running');
+
+  useEffect(() => {
+    setValid(audioContext && audioContext.state === 'running');
+  }, [audioContext]);
+
+  // TODO: [DS-71] @delude88 please check business logic
+  const start = useCallback(() => {
+    if (!audioContext) {
+      return createAudioContext().then((createdAudioContext) => {
+        if (createdAudioContext.state === 'suspended') {
+          return createdAudioContext.resume().then(() => {
+            if (createdAudioContext.state === 'running') setValid(true);
+          });
+        }
+        return null;
+      });
+    }
+    return audioContext.resume().then(() => {
+      if (audioContext.state === 'running') setValid(true);
+    });
+  }, [audioContext]);
 
   return (
     <Flex
@@ -21,7 +45,7 @@ const StageDeviceController = (): JSX.Element => {
         bottom: 0,
         left: '50%',
         transform: 'translate(-50%, 0)',
-        width: '228px',
+        width: '280px',
         justifyContent: 'space-between',
         pb: '1rem',
         zIndex: 10,
@@ -53,7 +77,15 @@ const StageDeviceController = (): JSX.Element => {
           {localDevice.sendAudio ? <FaMicrophone size="24px" /> : <FaMicrophoneSlash size="24px" />}
         </Button>
       )}
-      <FixedAudioPlaybackStarterButton />
+
+      <Button variant={valid ? 'circleGray' : 'circle'} onClick={() => start()}>
+        {valid ? (
+          <GiSpeakerOff size={24} name="Speaker off" />
+        ) : (
+          <GiSpeaker size={24} name="Speaker on" />
+        )}
+      </Button>
+
       <Link href="/leave">
         <Button variant="circle" title="Bühne verlassen" sx={{ bg: 'primary', color: 'text' }}>
           <ImPhoneHangUp size="24px" />
