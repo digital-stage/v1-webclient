869acef419ea6adcfc455f63b1fb575763ff4b67
delude88 <tobias.hegemann@googlemail.com>
2021-01-08 16:58:21 +0100

    fix: i18n domain handling

--- next.config.js
+++ next.config.js
@@ -8,15 +8,5 @@ module.exports = {
     i18n: {
         locales: ['de', 'en'],
         defaultLocale: 'de',
-        domains: [
-            {
-                domain: 'live.digital-stage.org',
-                defaultLocale: 'de'
-            },
-            {
-                domain: 'test.digital-stage.org',
-                defaultLocale: 'de'
-            }
-        ]
     }
 }
