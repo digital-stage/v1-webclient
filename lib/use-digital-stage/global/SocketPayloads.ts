8ec57cf3d12b8e1153ae69435b24a2147acca149
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-03-05 19:06:07 +0100

    Added label to sound card

--- lib/use-digital-stage/global/SocketPayloads.ts
+++ lib/use-digital-stage/global/SocketPayloads.ts
@@ -1,254 +1,260 @@
-// DEVICE
-import { ThreeDimensionAudioProperties } from '../types/ThreeDimensionAudioProperty';
+import {ThreeDimensionAudioProperties} from "../types";
+
+export interface StageManaged {
+    id: string;
+    ovServer: {
+        ipv4: string;
+        ipv6?: string;
+        port: number;
+        pin: number;
+    };
+}
+
+export type StageUnManaged = string;
 
 // DEVICE
 export interface AddAudioProducerPayload {
-  routerId: string;
-  routerProducerId: string;
+    routerId: string;
+    routerProducerId: string;
 }
+
 export type RemoveAudioProducerPayload = string;
 
 export interface AddVideoProducerPayload {
-  routerId: string;
-  routerProducerId: string;
+    routerId: string;
+    routerProducerId: string;
 }
 
 export interface ChangeVideoProducerPayload {
-  id: string;
-  producer: Partial<{
-    routerId: string;
-    routerProducerId: string;
-  }>;
+    id: string;
+    producer: Partial<{
+        routerId: string;
+        routerProducerId: string;
+    }>;
 }
 
 export type RemoveVideoProducerPayload = string;
 
 export interface AddSoundCardPayload {
-  id: string;
-  initial: {
     name: string;
-    driver: 'JACK' | 'ALSA' | 'ASIO' | 'WEBRTC';
+    label?: string;
+    driver: "JACK" | "ALSA" | "ASIO" | "WEBRTC";
     numInputChannels?: number;
     numOutputChannels?: number;
     trackPresetId?: string;
     sampleRate?: number;
     periodSize?: number;
     numPeriods?: number; // default to 2
-  };
 }
 
 export interface ChangeSoundCardPayload {
-  id: string;
-  update: Partial<{
-    name: string;
-    driver: 'JACK' | 'ALSA' | 'ASIO' | 'WEBRTC';
-    numInputChannels?: number;
-    numOutputChannels?: number;
-    trackPresetId?: string;
-    sampleRate?: number;
-    periodSize?: number;
-    numPeriods?: number; // default to 2
-  }>;
+    id: string;
+    update: Partial<{
+        name: string;
+        label: string;
+        driver: "JACK" | "ALSA" | "ASIO" | "WEBRTC";
+        numInputChannels?: number;
+        numOutputChannels?: number;
+        trackPresetId?: string;
+        sampleRate?: number;
+        periodSize?: number;
+        numPeriods?: number; // default to 2
+    }>;
 }
 
 export type RemoveSoundCardPayload = string;
 
 export interface AddTrackPresetPayload {
-  id: string;
-  initial: {
     soundCardId: string;
-    name: string;
-    outputChannels: number[];
-  };
+    name?: string;
+    inputChannels?: number[];
+    outputChannels?: number[];
 }
 
 export interface ChangeTrackPresetPayload {
-  id: string;
-  update: Partial<{
-    name: string;
-    outputChannels: number[];
-  }>;
+    id: string;
+    update: Partial<{
+        name: string;
+        inputChannels: number[];
+        outputChannels: number[];
+    }>;
 }
 
 export type RemoveTrackPresetPayload = string;
 
 export interface AddTrackPayload {
-  id: string;
-  initial: {
-    trackPresetId: string;
+    soundCardId: string;
     channel: number;
-    gain?: number;
-    volume?: number;
-    directivity: 'omni' | 'cardioid';
-  };
 }
 
 export interface ChangeTrackPayload {
-  id: string;
-  update: Partial<{
-    channel: number;
-    gain: number;
-    volume: number;
-    directivity: 'omni' | 'cardioid';
-  }>;
+    id: string;
+    update: Partial<{
+        channel: number;
+        gain: number;
+        volume: number;
+        directivity: "omni" | "cardioid";
+    }>;
 }
 
 export type RemoveTrackPayload = string;
 
 // STAGE
 export type AddStagePayload = Partial<{
-  name: string;
-  password?: string;
-  width?: number;
-  length?: number;
-  height?: number;
-  absorption?: number;
-  damping?: number;
-  admins?: string[];
+    name: string;
+    password?: string;
+    width?: number;
+    length?: number;
+    height?: number;
+    absorption?: number;
+    damping?: number;
+    admins?: string[];
 }>;
 
 export interface ChangeStagePayload {
-  id: string;
-  update: Partial<{
-    name: string;
-    password: string;
-    width: number;
-    length: number;
-    height: number;
-    absorption: number;
-    damping: number;
-    // admins: string[];
-  }>;
+    id: string;
+    update: Partial<{
+        name: string;
+        password: string;
+        width: number;
+        length: number;
+        height: number;
+        absorption: number;
+        damping: number;
+        // admins: string[];
+    }>;
 }
 
 export type RemoveStagePayload = string;
 
 // GROUP
-export interface AddGroupPayload extends Partial<ThreeDimensionAudioProperties> {
-  stageId: string;
-  name: string;
+export interface AddGroupPayload
+    extends Partial<ThreeDimensionAudioProperties> {
+    stageId: string;
+    name: string;
 }
 
 export interface ChangeGroupPayload {
-  id: string;
-  update: Partial<ThreeDimensionAudioProperties>;
+    id: string;
+    update: Partial<ThreeDimensionAudioProperties>;
 }
 
 export type RemoveGroupPayload = string;
 
 // STAGE MEMBER
 export interface ChangeStageMemberPayload {
-  id: string;
-  update: Partial<
-    {
-      isDirector: boolean;
-    } & ThreeDimensionAudioProperties
-  >;
+    id: string;
+    update: Partial<{
+        isDirector: boolean;
+    } & ThreeDimensionAudioProperties>;
 }
 
 export type RemoveStageMemberPayload = string;
 
 // STAGE MEMBER AUDIO PRODUCER
 export interface ChangeStageMemberAudioProducerPayload {
-  id: string;
-  update: Partial<ThreeDimensionAudioProperties>;
+    id: string;
+    update: Partial<ThreeDimensionAudioProperties>;
 }
 
 // STAGE MEMBER OV TRACK
 export interface ChangeStageMemberOvTrackPayload {
-  id: string;
-  update: Partial<ThreeDimensionAudioProperties>;
+    id: string;
+    update: Partial<ThreeDimensionAudioProperties>;
 }
 
 // CUSTOM GROUP
-export interface AddCustomGroupPayload extends Partial<ThreeDimensionAudioProperties> {
-  groupId: string;
+export interface AddCustomGroupPayload
+    extends Partial<ThreeDimensionAudioProperties> {
+    groupId: string;
 }
 
 export interface SetCustomGroupPayload {
-  groupId: string;
-  update: Partial<ThreeDimensionAudioProperties>;
+    groupId: string;
+    update: Partial<ThreeDimensionAudioProperties>;
 }
 
 export interface UpdateCustomGroupPayload {
-  id: string;
-  volume: number;
-  muted: boolean;
+    id: string;
+    volume: number;
+    muted: boolean;
 }
 
 export type RemoveCustomGroupPayload = string;
 
 // CUSTOM STAGE MEMBER
-export interface AddCustomStageMemberPayload extends Partial<ThreeDimensionAudioProperties> {
-  stageMemberId: string;
+export interface AddCustomStageMemberPayload
+    extends Partial<ThreeDimensionAudioProperties> {
+    stageMemberId: string;
 }
 
 export interface SetCustomStageMemberPayload {
-  stageMemberId: string;
-  update: Partial<ThreeDimensionAudioProperties>;
+    stageMemberId: string;
+    update: Partial<ThreeDimensionAudioProperties>;
 }
 
 export interface UpdateCustomStageMemberPayload {
-  id: string;
-  update: Partial<ThreeDimensionAudioProperties>;
+    id: string;
+    update: Partial<ThreeDimensionAudioProperties>;
 }
 
 export type RemoveCustomStageMemberPayload = string;
 
 // CUSTOM STAGE MEMBER AUDIO
-export interface AddCustomStageMemberAudioPayload extends Partial<ThreeDimensionAudioProperties> {
-  stageMemberAudioId: string;
+export interface AddCustomStageMemberAudioPayload
+    extends Partial<ThreeDimensionAudioProperties> {
+    stageMemberAudioId: string;
 }
 
 export interface SetCustomStageMemberAudioPayload {
-  stageMemberAudioId: string;
-  update: Partial<ThreeDimensionAudioProperties>;
+    stageMemberAudioId: string;
+    update: Partial<ThreeDimensionAudioProperties>;
 }
 
 export interface UpdateCustomStageMemberAudioPayload {
-  id: string;
-  update: Partial<ThreeDimensionAudioProperties>;
+    id: string;
+    update: Partial<ThreeDimensionAudioProperties>;
 }
 
 export type RemoveCustomStageMemberAudioPayload = string;
 
 // CUSTOM STAGE MEMBER OV
-export interface AddCustomStageMemberOvPayload extends Partial<ThreeDimensionAudioProperties> {
-  stageMemberOvTrackId: string;
-  gain: number;
-  directivity: 'omni' | 'cardioid';
+export interface AddCustomStageMemberOvPayload
+    extends Partial<ThreeDimensionAudioProperties> {
+    stageMemberOvTrackId: string;
+    gain: number;
+    directivity: "omni" | "cardioid";
 }
 
 export interface SetCustomStageMemberOvPayload {
-  stageMemberOvTrackId: string;
-  update: Partial<ThreeDimensionAudioProperties>;
+    stageMemberOvTrackId: string;
+    update: Partial<ThreeDimensionAudioProperties>;
 }
 
 export interface UpdateCustomStageMemberOvPayload {
-  id: string;
-  update: Partial<ThreeDimensionAudioProperties>;
+    id: string;
+    update: Partial<ThreeDimensionAudioProperties>;
 }
 
 export type RemoveCustomStageMemberOvPayload = string;
 
 // JOIN STAGE
 export interface JoinStagePayload {
-  stageId: string;
-  groupId: string;
-  password?: string;
+    stageId: string;
+    groupId: string;
+    password?: string;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface LeaveStagePayload {}
+export interface LeaveStagePayload {
+}
 
 export type LeaveStageForGoodPayload = string;
-
-export type SendMessagePayload = string;
 
 export type SendChatMessagePayload = string;
 
 // User
 export interface ChangeUserPayload {
-  name: string;
-  avatarUrl?: string;
+    name: string;
+    avatarUrl?: string;
 }
--- lib/use-digital-stage/types/SoundCard.ts
+++ lib/use-digital-stage/types/SoundCard.ts
@@ -1,10 +1,11 @@
-import {DeviceId, SoundCardId, UserId} from "./IdTypes";
+import { DeviceId, SoundCardId, UserId } from "./IdTypes";
 
 export interface SoundCard {
   // ov-specific
   _id: SoundCardId;
   deviceId: DeviceId;
   name: string; // unique together with userId
+  label: string;
 
   isDefault?: boolean;
 
@@ -20,12 +21,11 @@ export interface SoundCard {
   numInputChannels: number;
   numOutputChannels: number;
 
-  inputChannels: number[];  //TODO: Replace this later by track presets
+  inputChannels: number[]; // TODO: Replace this later by track presets
   outputChannels: number[]; // Will be 0 and 1 per default TODO: Later let user decide
 
-
-  //TODO: For later we should use track presets
-  //trackPresetId?: TrackPresetId; // Current default preset (outside or on new stages)
+  // TODO: For later we should use track presets
+  // trackPresetId?: TrackPresetId; // Current default preset (outside or on new stages)
 
   // Optimizations for performance
   // trackPresets: TrackPresetId[];
